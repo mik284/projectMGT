8b8afaf9cb6ba49485b06b9487c39df47c4009bc
mik284 <mikeykamau222@gmail.com>
2024-06-09 15:15:28 +0300

    handle events *description

--- src/main/java/com/company/projectmgt/view/taskdetail/TaskDetailView.java
+++ src/main/java/com/company/projectmgt/view/taskdetail/TaskDetailView.java
@@ -2,9 +2,12 @@ package com.company.projectmgt.view.taskdetail;
 
 
 import com.company.projectmgt.entity.Task;
+import com.company.projectmgt.entity.TaskPriority;
 import com.company.projectmgt.view.main.MainView;
 
+import com.vaadin.flow.component.AbstractField;
 import com.vaadin.flow.router.Route;
+import io.jmix.flowui.component.textarea.JmixTextArea;
 import io.jmix.flowui.view.*;
 
 @Route(value = "tasks/:id", layout = MainView.class)
@@ -13,4 +16,13 @@ import io.jmix.flowui.view.*;
 @EditedEntityContainer("taskDc")
 @DialogMode(width = "AUTO", height = "AUTO")
 public class TaskDetailView extends StandardDetailView <Task> {
+    @Subscribe
+    public void onInitEntity(final InitEntityEvent<Task> event) {
+        event.getEntity().setPriority(TaskPriority.MEDIUM);
+    }
+
+    @Subscribe("descriptionField")
+    public void onDescriptionFieldComponentValueChange(final AbstractField.ComponentValueChangeEvent<JmixTextArea, ?> event) {
+        event.getSource().setHelperText(event.getValue().toString().length()+"/"+500);
+    }
 }
