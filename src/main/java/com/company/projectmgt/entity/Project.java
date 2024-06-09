609dbeb58097f8de4e4f7ae9a0f9434f4d354b05
mik284 <mikeykamau222@gmail.com>
2024-06-09 12:44:15 +0300

    defined data models and their relationships

---
+++ src/main/java/com/company/projectmgt/entity/Project.java
@@ -0,0 +1,80 @@
+package com.company.projectmgt.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.Composition;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "PROJECT", indexes = {
+        @Index(name = "IDX_PROJECT_MANAGER", columnList = "MANAGER_ID")
+})
+@Entity
+public class Project {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @InstanceName
+    @Column(name = "NAME", nullable = false)
+    @NotNull
+    private String name;
+
+    @JoinColumn(name = "MANAGER_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User manager;
+
+    @Column(name = "DESCRITPTION")
+    @Lob
+    private String descritption;
+
+    @Composition
+    @OneToMany(mappedBy = "project")
+    private List<Task> tasks;
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public String getDescritption() {
+        return descritption;
+    }
+
+    public void setDescritption(String descritption) {
+        this.descritption = descritption;
+    }
+
+    public User getManager() {
+        return manager;
+    }
+
+    public void setManager(User manager) {
+        this.manager = manager;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
