034fa4b533fb0941998a286139d580d058e20447
mik284 <mikeykamau222@gmail.com>
2024-06-14 15:54:05 +0300

    modify new bean

--- src/main/java/com/company/projectmgt/app/NewBean.java
+++ src/main/java/com/company/projectmgt/app/NewBean.java
@@ -1,7 +1,11 @@
 package com.company.projectmgt.app;
 
+import com.company.projectmgt.entity.Task;
 import io.jmix.core.DataManager;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Component
 public class NewBean {
@@ -9,5 +13,16 @@ public class NewBean {
 
     public NewBean(DataManager dataManager) {
         this.dataManager = dataManager;
+    }
+
+    public List<Task> loadTasksByQuery(){
+        final List<Task> myEntityList = dataManager.load(Task.class)
+                .query("select t from Task_ t " +
+                        "where extract(month from t.dueDate) = :month " +
+                        "order by t.name asc")
+                .parameter("month", LocalDate.now().getMonthValue())
+                .list();
+
+        return myEntityList;
     }
 }
